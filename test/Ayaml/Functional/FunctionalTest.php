510aeea711f06d703c3e6c351326115c757b8787
gong023 <gon.gong.gone@gmail.com>
2014-12-29 00:48:36 +0900

    add date tests

--- test/Ayaml/Functional/FunctionalTest.php
+++ test/Ayaml/Functional/FunctionalTest.php
@@ -65,4 +65,55 @@ class FunctionalTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals($expected, $actual);
     }
+
+    /**
+     * @test
+     */
+    public function incrementDate()
+    {
+        $actual = Ayaml::seq($this->container)->between('created', '2014-01', '2014-03')->byMonth()->dump();
+
+        $expected = [
+            ['id' => 1, 'name' => 'Taro', 'created' => '2014-01-01 00:00:00'],
+            ['id' => 1, 'name' => 'Taro', 'created' => '2014-02-01 00:00:00'],
+            ['id' => 1, 'name' => 'Taro', 'created' => '2014-03-01 00:00:00'],
+        ];
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * @test
+     */
+    public function decrementDate()
+    {
+        $actual = Ayaml::seq($this->container)->between('created', '2014-01', '2013-11')->byMonth()->dump();
+
+        $expected = [
+            ['id' => 1, 'name' => 'Taro', 'created' => '2014-01-01 00:00:00'],
+            ['id' => 1, 'name' => 'Taro', 'created' => '2013-12-01 00:00:00'],
+            ['id' => 1, 'name' => 'Taro', 'created' => '2013-11-01 00:00:00'],
+        ];
+
+        $this->assertEquals($expected, $actual);
+    }
+
+//    /**
+//     * @test
+//     */
+//    public function complex()
+//    {
+//        $actual = Ayaml::seq($this->container)
+//            ->range('id', 10, 13)->byOne()
+//            ->between('created', '2014-01', '2014-03')->byMonth()
+//            ->dump();
+//
+//        $expected = [
+//            ['id' => 10, 'name' => 'Taro', 'created' => '2014-01-01 00:00:00'],
+//            ['id' => 11, 'name' => 'Taro', 'created' => '2014-02-01 00:00:00'],
+//            ['id' => 12, 'name' => 'Taro', 'created' => '2014-03-01 00:00:00'],
+//        ];
+//
+//        $this->assertEquals($expected, $actual);
+//    }
 }
---
+++ test/Ayaml/Unit/Sequence/Calculator/Datetime/IncrementerTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Ayaml\Test\Unit\Sequence\Calculator\Datetime;
+
+use AssertChain\AssertChain;
+use Ayaml\Ayaml;
+
+class IncrementerTest extends \PHPUnit_Framework_TestCase
+{
+    use AssertChain;
+
+    /**
+     * @var \Ayaml\ContainerCollection
+     */
+    private $subject;
+
+    public function setUp()
+    {
+        $container = Ayaml::file('user')->schema('valid_user');
+        $this->subject = Ayaml::seq($container);
+    }
+
+    public function testByDay()
+    {
+        $days = $this->subject->between('created', '2014-01-01 00:00:00', '2014-01-03 00:00:00')->byDay();
+
+        $this->centralizedAssert($days)
+            ->beInstanceOf('\Ayaml\ContainerCollection')
+            ->attributeContainsOnly('\Ayaml\Container', 'containers')
+            ->attributeCount(3, 'containers');
+    }
+
+//    public function testFormatting()
+//    {
+//        $days = $this->subject->between('created', '2014-01-01 00:00:00', '2014-01-03 00:00:00')->byDay('Y-m-d')->dump();
+//
+//        $expected = [
+//            ['id' => 1, 'name' => 'Taro', 'created' => '2014-01-01'],
+//            ['id' => 1, 'name' => 'Taro', 'created' => '2014-01-02'],
+//            ['id' => 1, 'name' => 'Taro', 'created' => '2014-01-03'],
+//        ];
+//
+//        $this->assertEquals($expected, $days);
+//    }
+
+    public function testByWeek()
+    {
+        $days = $this->subject->between('created', '2014-01-01 00:00:00', '2014-01-21 00:00:00')->byWeek();
+
+        $this->centralizedAssert($days)
+            ->beInstanceOf('\Ayaml\ContainerCollection')
+            ->attributeContainsOnly('\Ayaml\Container', 'containers')
+            ->attributeCount(3, 'containers');
+    }
+
+    public function testByMonth()
+    {
+        $days = $this->subject->between('created', '2014-01-01 00:00:00', '2014-03-01 00:00:00')->byMonth();
+
+        $this->centralizedAssert($days)
+            ->beInstanceOf('\Ayaml\ContainerCollection')
+            ->attributeContainsOnly('\Ayaml\Container', 'containers')
+            ->attributeCount(3, 'containers');
+
+    }
+
+    public function testByYear()
+    {
+        $days = $this->subject->between('created', '2014-01-01 00:00:00', '2016-01-01 00:00:00')->byYear();
+
+        $this->centralizedAssert($days)
+            ->beInstanceOf('\Ayaml\ContainerCollection')
+            ->attributeContainsOnly('\Ayaml\Container', 'containers')
+            ->attributeCount(3, 'containers');
+
+    }
+}
+
